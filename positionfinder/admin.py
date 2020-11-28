9d54abbb423c78df25b46dec493d69482e47602e
hybridpicker <lukas@schoensgibl.com>
2020-11-28 11:09:24 +0100

    melodic minor scales

--- positionfinder/admin.py
+++ positionfinder/admin.py
@@ -3,9 +3,9 @@ from .models import Notes, Root, NotesCategory
 from .positions import NotesPosition
 from .models_chords import ChordNotes, ChordPosition
 
-#admin.site.register(Root)
-#admin.site.register(Notes)
-#admin.site.register(NotesCategory)
-#admin.site.register(NotesPosition)
-#admin.site.register(ChordNotes)
-#admin.site.register(ChordPosition)
+admin.site.register(Root)
+admin.site.register(Notes)
+admin.site.register(NotesCategory)
+admin.site.register(NotesPosition)
+admin.site.register(ChordNotes)
+admin.site.register(ChordPosition)
