052c7bef42d734e697982852adc413a95cfdf839
hybridpicker <lukas@schoensgibl.com>
2024-05-17 08:21:53 +0200

    base setup sidebard

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -2,33 +2,28 @@ document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.getElementById('menuToggle');
     const menuCheckbox = document.getElementById('menuCheckbox');
     const menu = document.getElementById('menu');
+    const leftCursor = document.querySelector('.left-cursor');
     
     if (menuToggle && menuCheckbox && menu) {
-      console.log("Checkbox and menu found");
-  
-      menuToggle.addEventListener('click', function(event) {
-        console.log("Menu toggle clicked");
-        event.stopPropagation(); // Verhindert das Auslösen des Events auf dem Dokument
-        menuCheckbox.checked = !menuCheckbox.checked; // Toggle checkbox state
-        if (menuCheckbox.checked) {
-          console.log("Checkbox is checked");
-          menu.style.left = '0';
-        } else {
-          console.log("Checkbox is not checked");
-          menu.style.left = '-300px';
-        }
-        console.log("Menu style left: ", menu.style.left);
-      });
-  
-      document.addEventListener('click', function() {
-        if (menuCheckbox.checked) {
-          console.log("Click outside menu");
-          menuCheckbox.checked = false;
-          menu.style.left = '-300px';
-        }
-      });
-    } else {
-      console.log("Checkbox or menu not found");
+        menuToggle.addEventListener('click', function(event) {
+            event.stopPropagation(); // Prevent the event from triggering on the document
+            menuCheckbox.checked = !menuCheckbox.checked; // Toggle checkbox state
+            if (menuCheckbox.checked) {
+                menu.style.left = '0';
+                leftCursor.style.zIndex = 'unset';
+            } else {
+                menu.style.left = '-300px';
+                leftCursor.style.zIndex = '10';
+            }
+        });
+
+        document.addEventListener('click', function(event) {
+            // Only close the menu if clicking outside of it
+            if (menuCheckbox.checked && !menuToggle.contains(event.target) && !menu.contains(event.target)) {
+                menuCheckbox.checked = false;
+                menu.style.left = '-300px';
+                leftCursor.style.zIndex = '10';
+            }
+        });
     }
-  });
-  
+});
